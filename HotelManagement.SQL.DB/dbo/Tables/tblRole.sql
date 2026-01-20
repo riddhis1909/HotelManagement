8d104d14a17e012e26b126005dee859ef52bc79b
Riddhis1909 <riddhi.shah295@outlook.com>
2026-01-20 17:51:42 +0530

    #Created tblRole and tblUser in DB

---
+++ HotelManagement.SQL.DB/dbo/Tables/tblRole.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblRole] (
+    [RoleID]       INT           IDENTITY (1, 1) NOT NULL,
+    [RoleName]     VARCHAR (15)  NOT NULL,
+    [Description]  VARCHAR (MAX) NULL,
+    [IsRoleActive] BIT           CONSTRAINT [DF_tblRole_IsRoleActive] DEFAULT ((0)) NOT NULL,
+    [CreatedBy]    INT           NULL,
+    [CreatedDate]  DATETIME      NULL,
+    [UpdatedBy]    INT           NULL,
+    [UpdatedDate]  DATETIME      NULL,
+    [DeletedBy]    INT           NULL,
+    [DeletedDate]  DATETIME      NULL,
+    CONSTRAINT [PK_tblRole] PRIMARY KEY CLUSTERED ([RoleID] ASC)
+);
+
---
+++ HotelManagement.SQL.DB/dbo/Tables/tblUser.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[tblUser] (
+    [UserID]          INT           IDENTITY (1, 1) NOT NULL,
+    [RoleID]          INT           NOT NULL,
+    [FirstName]       VARCHAR (20)  NOT NULL,
+    [LastName]        VARCHAR (20)  NOT NULL,
+    [EmailID]         VARCHAR (50)  NOT NULL,
+    [MobileNumber]    VARCHAR (20)  NOT NULL,
+    [Address]         VARCHAR (MAX) NULL,
+    [City]            VARCHAR (20)  NULL,
+    [ZipCode]         VARCHAR (15)  NULL,
+    [StateID]         INT           NULL,
+    [Password]        VARCHAR (MAX) NULL,
+    [IsProfileActive] BIT           CONSTRAINT [DF_tblUser_IsProfileActive] DEFAULT ((0)) NOT NULL,
+    [CreatedBy]       INT           NULL,
+    [CreatedDate]     DATETIME      NULL,
+    [UpdatedBy]       INT           NULL,
+    [UpdatedDate]     DATETIME      NULL,
+    [DeletedBy]       INT           NULL,
+    [DeletedDate]     DATETIME      NULL,
+    [LastLoginDate]   DATETIME      NULL,
+    CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED ([UserID] ASC),
+    CONSTRAINT [FK_tblUser_tblRole] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[tblRole] ([RoleID])
+);
+
